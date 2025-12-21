574e5dc566550150681f1d28bebd2cb302656692
ykchiu2486 <roger2486.cs13@nycu.edu.tw>
2025-12-21 09:53:36 +0800

    feat: add Category, Whishlist, Transaction, and Message models to database schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -29,6 +29,7 @@ class Item(Base):
     status = Column(Boolean, nullable=False, default=True)  # 0: unavailable, 1: available
     desired_item = Column(String, nullable=True)  # for exchange items
     total_images = Column(Integer, default=0, nullable=False)
+    category = Column(ForeignKey("categories.category_id"), Integer, nullable=False)
 
 class ItemImage(Base):
     __tablename__ = "item_images"
@@ -36,3 +37,43 @@ class ItemImage(Base):
     image_id = Column(Integer, primary_key=True, index=True)
     item_id = Column(Integer, ForeignKey("items.item_id", ondelete="CASCADE"), nullable=False)
     image_data = Column(LargeBinary, nullable=False)
+
+class Category(Base): # delete parent category idea
+    __tablename__ = "categories"
+
+    category_id = Column(Integer, primary_key=True, index=True)
+    category_name = Column(String, unique=True, nullable=False)
+
+class Whishlist(Base):
+    __tablename__ = "whishlist"
+
+    whishlist_id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    item_id = Column(Integer, ForeignKey("items.item_id", ondelete="CASCADE"), nullable=False)
+    added_date = Column(DateTime(timezone=True), server_default=func.now())
+
+class Transaction(Base):
+    __tablename__ = "transactions"
+
+    transaction_id = Column(Integer, primary_key=True, index=True)
+    seller_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    buyer_id = Column(Integer, ForeignKey("users.user_id", ondelete="SET NULL"), nullable=True)
+    item_id = Column(Integer, ForeignKey("items.item_id", ondelete="SET NULL"), nullable=True)
+    transaction_date = Column(DateTime(timezone=True), server_default=func.now())
+    final_price = Column(Integer, nullable=True)
+    rating = Column(Integer, nullable=True)  # rating given by buyer to seller
+    review = Column(Text, nullable=True)  # review given by buyer to seller
+    status = Column(String, nullable=False, default="pending")  # pending, completed, cancelled
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    message_id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    receiver_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable=False)
+    item_id = Column(Integer, ForeignKey("items.item_id", ondelete="CASCADE"), nullable=False)
+    content = Column(Text, nullable=False)
+    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+
+
+    
